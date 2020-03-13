b3fa040b94493de0b01056a469921e02492f5501
Sanoop <sanoop@finbox.in>
2020-03-13 19:08:12 +0530

    Added logs to buil script

--- scripts/androidCreatePropertiesFile.js
+++ scripts/androidCreatePropertiesFile.js
@@ -1,20 +1,23 @@
 #!/usr/bin/env node
 
 module.exports = function (context) {
-
+    console.log('Entering abdroid script !!!');
     var fs = context.requireCordovaModule('fs'),
         path = context.requireCordovaModule('path');
-
     const PLUGIN_NAME = "cordova-plugin-finbox-risk-manager";
     var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
     var GRADLE_FILENAME = path.join(platformRoot, 'finbox.properties');
+    console.log('Platform root -> ', platformRoot);
+    console.log('Gradle file name -> ', GRADLE_FILENAME);
     // const GRADLE_FILENAME = path.resolve(process.cwd(), 'platforms', 'android', 'finbox.properties');
 
     fs.readFile(path.resolve(context.opts.projectRoot, 'package.json'), function (err, data) {
         var jsonData = JSON.parse(data)
+        console.log('Package json content -> ', jsonData);
         if (jsonData.cordova.platforms != "android") return
         var plugins = jsonData.cordova.plugins
         var requiredPlugin = plugins[PLUGIN_NAME]
+        console.log('Required plugin -> ', requiredPlugin);
         if (requiredPlugin != null) {
             var awsKey = requiredPlugin.AWS_KEY
             var awsSecret = requiredPlugin.AWS_SECRET
@@ -32,6 +35,7 @@ FINBOX_RM_ARTIFACT=${finboxRmArtifact}`
     * Write finbox.properties with:
     */
     function setGradleTemplate(template) {
+        console.log("Seting gradle template -> ", template);
         fs.writeFile(GRADLE_FILENAME, template, 'utf8', function (err) {
             if (err) return console.log(PLUGIN_NAME, " FAILED TO WRITE ", GRADLE_FILENAME, " > ", template, err);
         });
