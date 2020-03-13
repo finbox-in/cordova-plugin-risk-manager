d67a9afd8767a586838dddb24d5166d0530e7e81
Sanoop <sanoop@finbox.in>
2020-03-13 17:06:57 +0530

    Change path to use context

--- scripts/androidCreatePropertiesFile.js
+++ scripts/androidCreatePropertiesFile.js
@@ -1,34 +1,39 @@
-var fs = require('fs');
-var path = require('path');
+#!/usr/bin/env node
 
-const PLUGIN_NAME = "cordova-plugin-finbox-risk-manager";
-const GRADLE_FILENAME = path.resolve(process.cwd(), 'platforms', 'android', 'finbox.properties');
+module.exports = function (context) {
 
-fs.readFile(path.resolve(process.cwd(), 'package.json'), function (err, data) {
-    var jsonData = JSON.parse(data)
-    if (jsonData.cordova.platforms != "android") return
-    var plugins = jsonData.cordova.plugins
-    var requiredPlugin = plugins[PLUGIN_NAME]
-    if (requiredPlugin != null) {
-        var awsKey = requiredPlugin.AWS_KEY
-        var awsSecret = requiredPlugin.AWS_SECRET
-        var finboxRmVersion = requiredPlugin.FINBOX_RM_VERSION
-        var finboxRmArtifact = requiredPlugin.FINBOX_RM_ARTIFACT
-        var gradleTemplate = `AWS_KEY=${awsKey}
-AWS_SECRET=${awsSecret}
-FINBOX_RM_VERSION=${finboxRmVersion}
-FINBOX_RM_ARTIFACT=${finboxRmArtifact}`
-        setGradleTemplate(gradleTemplate)
-    }
-});
+    var fs = require('fs'),
+        path = require('path');
 
-/**
-* Write finbox.properties with:
-*
-*
-*/
-function setGradleTemplate(template) {
-    fs.writeFile(GRADLE_FILENAME, template, 'utf8', function (err) {
-        if (err) return console.log(PLUGIN_NAME, " FAILED TO WRITE ", GRADLE_FILENAME, " > ", template, err);
+    const PLUGIN_NAME = "cordova-plugin-finbox-risk-manager";
+    var platformRoot = path.join(context.opts.projectRoot, 'platforms/android');
+    var GRADLE_FILENAME = path.join(platformRoot, 'finbox.properties');
+    // const GRADLE_FILENAME = path.resolve(process.cwd(), 'platforms', 'android', 'finbox.properties');
+
+    fs.readFile(path.resolve(process.cwd(), 'package.json'), function (err, data) {
+        var jsonData = JSON.parse(data)
+        if (jsonData.cordova.platforms != "android") return
+        var plugins = jsonData.cordova.plugins
+        var requiredPlugin = plugins[PLUGIN_NAME]
+        if (requiredPlugin != null) {
+            var awsKey = requiredPlugin.AWS_KEY
+            var awsSecret = requiredPlugin.AWS_SECRET
+            var finboxRmVersion = requiredPlugin.FINBOX_RM_VERSION
+            var finboxRmArtifact = requiredPlugin.FINBOX_RM_ARTIFACT
+            var gradleTemplate = `AWS_KEY=${awsKey}
+    AWS_SECRET=${awsSecret}
+    FINBOX_RM_VERSION=${finboxRmVersion}
+    FINBOX_RM_ARTIFACT=${finboxRmArtifact}`
+            setGradleTemplate(gradleTemplate)
+        }
     });
+
+    /**
+    * Write finbox.properties with:
+    */
+    function setGradleTemplate(template) {
+        fs.writeFile(GRADLE_FILENAME, template, 'utf8', function (err) {
+            if (err) return console.log(PLUGIN_NAME, " FAILED TO WRITE ", GRADLE_FILENAME, " > ", template, err);
+        });
+    }
 }
